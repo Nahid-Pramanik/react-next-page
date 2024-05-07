a89b92602c441b805a5744815342b80cc8f51707
unknown <niamulhuda9010@gmail.com>
2024-05-07 23:37:04 +0600

    book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,15 +2,21 @@ import PropTypes from 'prop-types';
 
 const Book = ({book}) => {
     console.log(book)
+    const {title, image, subtitle, price } = book;
     return (
-        <div>
-            <img src={book.image} alt="" />
+        <div className='overflow-hidden relative rounded-lg transition hover:-translate-y-1 duration-200'>
+            <img src={image} alt="Cover page Image"  className='object-cover h-48 md:h-64 lg:h-80 '/>
+            <div className='bg-black text-gray-300 bg-opacity-75 absolute inset-0 p-5 flex flex-col opacity-0 hover:opacity-100'>
+                <p>{title}</p>
+                <p className='mt-8'>{subtitle}</p>
+                <p className='mt-auto'>{price}</p>
+            </div>
         </div>
     );
 };
 
-Book.proptypes = {
-    book: PropTypes.object.isRequired,
+Book.propTypes = {
+    book: PropTypes.object
 }
 
 export default Book;
--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -5,7 +5,7 @@ import Book from "./Book";
 const Books = () => {
     const {books} = useLoaderData();
     return (
-        <div>
+        <div className="my-container grid  grid-cols-2 md:grid-cols-4 gap-3 md:gap-5 lg:gap-7 justify-items-center">
             {
                 books.map(book => <Book key={book.isbn13} book={book}></Book>)
             }
